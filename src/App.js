b7bb6aede4d546cdceb5c08b4745238d8298a18f
Grey Pegram <dpegram85@gmail.com>
2024-07-22 08:14:26 -0400

    Completed Lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <>
+      <div>
+        <h1>Hi, I'm Grey</h1>
+      </div>
+      <div>
+        <img src="./grey2.jpg" alt="Profile Picture of Grey" aria-label="Profile Picture of the Website Author"></img>
+      </div>
+      <div>
+        <h2>About Me</h2>
+        <p>
+          I'm a coding student, life-long gaming enthusiast, and retired
+          professional chef. I'm looking to merge my recipe crafting skills with gaming
+          development to cook up unique, delectable and innovative experiences.
+        </p>
+        <a href='https://github.com/gpegram85/' alt="GitHub Profile Link">GitHub</a>
+        <a href="https://www.linkedin.com/in/grey-pegram-75191b2a5/" alt="LinkedIn Profile Link">LinkedIn</a>
+      </div>
+    </>
+  )
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,67 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm _____`", () => {
+    render(<App />)
+
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false, 
+        level: 1,
+    })
+
+    expect(topLevelHeading).toBeInTheDocument()
+})
+
+test("displays a profile image and verifies an alt attribute", () => {
+    render(<App />)
+
+    const profileImage = screen.getByRole("img", {
+        name: /profile picture/i,
+    })
+
+    expect(profileImage).toBeInTheDocument()
+    expect(profileImage).toHaveAttribute('alt', `Profile Picture of Grey`)
+})
+
+test("displays a second-level heading with the text `About Me`", () => {
+    render(<App />)
+
+    const secondLevelHeading = screen.getByRole("heading", {
+        name: /about me/i,
+        exact: false,
+        level: 2
+    })
+
+    expect(secondLevelHeading).toBeInTheDocument()
+})
+
+test("displays a paragraph element containing a short biography", () => {
+    render(<App />)
+
+    const bioParagraph = screen.getByText(/i'm a coding student/i)
+
+    expect(bioParagraph).toBeInTheDocument()
+})
+
+test("displays a link to the authors github", () => {
+    render(<App />)
+
+    const githubLink = screen.getByRole("link", {
+        name: /github/i,
+        exact: false
+    })
+
+    expect(githubLink).toBeInTheDocument()
+})
+
+test("displays a link to the authors linkedin", () => {
+    render(<App />)
+
+    const linkedinLink = screen.getByRole("link", {
+        name: /linkedin/i,
+        exact: false
+    })
+
+    expect(linkedinLink).toBeInTheDocument()
+})
